718e330615815ac351f8cd8e813b7b3a81d5a1e2
Vaslot1 <102205372+Vaslot1@users.noreply.github.com>
2022-10-19 00:16:24 +0800

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,13 +45,17 @@
             this.labelHR = new System.Windows.Forms.Label();
             this.bnSolve = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.colorShower = new System.Windows.Forms.PictureBox();
+            this.colorShowerStart = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.colorShower)).BeginInit();
+            this.colorShowerRes = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.colorShowerStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorShowerRes)).BeginInit();
             this.SuspendLayout();
             // 
             // txtH
             // 
+            this.txtH.AcceptsReturn = true;
             this.txtH.Location = new System.Drawing.Point(12, 12);
             this.txtH.Name = "txtH";
             this.txtH.Size = new System.Drawing.Size(100, 23);
@@ -79,27 +83,27 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(118, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 15);
+            this.label1.Size = new System.Drawing.Size(57, 15);
             this.label1.TabIndex = 3;
-            this.label1.Text = "H";
+            this.label1.Text = "H [0..360]";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(118, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.Size = new System.Drawing.Size(54, 15);
             this.label2.TabIndex = 4;
-            this.label2.Text = "S";
+            this.label2.Text = "S [0..100]";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(118, 73);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.Size = new System.Drawing.Size(55, 15);
             this.label3.TabIndex = 5;
-            this.label3.Text = "V";
+            this.label3.Text = "V [0..100]";
             // 
             // cmbOperation
             // 
@@ -107,15 +111,17 @@
             this.cmbOperation.Items.AddRange(new object[] {
             "+",
             "-"});
-            this.cmbOperation.Location = new System.Drawing.Point(162, 36);
+            this.cmbOperation.Location = new System.Drawing.Point(188, 33);
             this.cmbOperation.Name = "cmbOperation";
+            this.cmbOperation.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.cmbOperation.Size = new System.Drawing.Size(37, 23);
             this.cmbOperation.TabIndex = 6;
+            this.cmbOperation.TabStop = false;
             this.cmbOperation.Text = "+";
             // 
             // txtTerm
             // 
-            this.txtTerm.Location = new System.Drawing.Point(205, 36);
+            this.txtTerm.Location = new System.Drawing.Point(231, 33);
             this.txtTerm.Name = "txtTerm";
             this.txtTerm.Size = new System.Drawing.Size(100, 23);
             this.txtTerm.TabIndex = 7;
@@ -130,16 +136,17 @@
             "B",
             "S",
             "V"});
-            this.cmbTypes.Location = new System.Drawing.Point(311, 36);
+            this.cmbTypes.Location = new System.Drawing.Point(337, 33);
             this.cmbTypes.Name = "cmbTypes";
             this.cmbTypes.Size = new System.Drawing.Size(34, 23);
             this.cmbTypes.TabIndex = 8;
+            this.cmbTypes.TabStop = false;
             this.cmbTypes.Text = "R";
             // 
             // txtResH
             // 
             this.txtResH.Enabled = false;
-            this.txtResH.Location = new System.Drawing.Point(377, 12);
+            this.txtResH.Location = new System.Drawing.Point(388, 12);
             this.txtResH.Name = "txtResH";
             this.txtResH.Size = new System.Drawing.Size(100, 23);
             this.txtResH.TabIndex = 9;
@@ -147,7 +154,7 @@
             // txtResS
             // 
             this.txtResS.Enabled = false;
-            this.txtResS.Location = new System.Drawing.Point(377, 44);
+            this.txtResS.Location = new System.Drawing.Point(388, 44);
             this.txtResS.Name = "txtResS";
             this.txtResS.Size = new System.Drawing.Size(100, 23);
             this.txtResS.TabIndex = 10;
@@ -155,7 +162,7 @@
             // txtResV
             // 
             this.txtResV.Enabled = false;
-            this.txtResV.Location = new System.Drawing.Point(377, 73);
+            this.txtResV.Location = new System.Drawing.Point(388, 73);
             this.txtResV.Name = "txtResV";
             this.txtResV.Size = new System.Drawing.Size(100, 23);
             this.txtResV.TabIndex = 11;
@@ -163,33 +170,33 @@
             // labelVB
             // 
             this.labelVB.AutoSize = true;
-            this.labelVB.Location = new System.Drawing.Point(486, 76);
+            this.labelVB.Location = new System.Drawing.Point(497, 76);
             this.labelVB.Name = "labelVB";
-            this.labelVB.Size = new System.Drawing.Size(14, 15);
+            this.labelVB.Size = new System.Drawing.Size(55, 15);
             this.labelVB.TabIndex = 14;
-            this.labelVB.Text = "V";
+            this.labelVB.Text = "V [0..100]";
             // 
             // labelSG
             // 
             this.labelSG.AutoSize = true;
-            this.labelSG.Location = new System.Drawing.Point(486, 47);
+            this.labelSG.Location = new System.Drawing.Point(497, 47);
             this.labelSG.Name = "labelSG";
-            this.labelSG.Size = new System.Drawing.Size(13, 15);
+            this.labelSG.Size = new System.Drawing.Size(54, 15);
             this.labelSG.TabIndex = 13;
-            this.labelSG.Text = "S";
+            this.labelSG.Text = "S [0..100]";
             // 
             // labelHR
             // 
             this.labelHR.AutoSize = true;
-            this.labelHR.Location = new System.Drawing.Point(483, 15);
+            this.labelHR.Location = new System.Drawing.Point(494, 15);
             this.labelHR.Name = "labelHR";
-            this.labelHR.Size = new System.Drawing.Size(16, 15);
+            this.labelHR.Size = new System.Drawing.Size(57, 15);
             this.labelHR.TabIndex = 12;
-            this.labelHR.Text = "H";
+            this.labelHR.Text = "H [0..360]";
             // 
             // bnSolve
             // 
-            this.bnSolve.Location = new System.Drawing.Point(162, 68);
+            this.bnSolve.Location = new System.Drawing.Point(188, 65);
             this.bnSolve.Name = "bnSolve";
             this.bnSolve.Size = new System.Drawing.Size(183, 23);
             this.bnSolve.TabIndex = 15;
@@ -199,7 +206,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(162, 97);
+            this.button1.Location = new System.Drawing.Point(188, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(183, 23);
             this.button1.TabIndex = 16;
@@ -207,29 +214,50 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // colorShower
+            // colorShowerStart
             // 
-            this.colorShower.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.colorShower.Location = new System.Drawing.Point(162, 134);
-            this.colorShower.Name = "colorShower";
-            this.colorShower.Size = new System.Drawing.Size(183, 50);
-            this.colorShower.TabIndex = 17;
-            this.colorShower.TabStop = false;
+            this.colorShowerStart.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.colorShowerStart.Location = new System.Drawing.Point(12, 134);
+            this.colorShowerStart.Name = "colorShowerStart";
+            this.colorShowerStart.Size = new System.Drawing.Size(163, 50);
+            this.colorShowerStart.TabIndex = 17;
+            this.colorShowerStart.TabStop = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(67, 150);
+            this.label4.Location = new System.Drawing.Point(422, 116);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(89, 15);
             this.label4.TabIndex = 18;
             this.label4.Text = "Итоговый цвет";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // colorShowerRes
+            // 
+            this.colorShowerRes.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.colorShowerRes.Location = new System.Drawing.Point(388, 134);
+            this.colorShowerRes.Name = "colorShowerRes";
+            this.colorShowerRes.Size = new System.Drawing.Size(158, 50);
+            this.colorShowerRes.TabIndex = 19;
+            this.colorShowerRes.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 15);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Начальный цвет";
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(518, 196);
+            this.ClientSize = new System.Drawing.Size(558, 196);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.colorShowerRes);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.colorShower);
+            this.Controls.Add(this.colorShowerStart);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.bnSolve);
             this.Controls.Add(this.labelVB);
@@ -247,9 +275,11 @@
             this.Controls.Add(this.txtV);
             this.Controls.Add(this.txtS);
             this.Controls.Add(this.txtH);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Конвертер цветов";
-            ((System.ComponentModel.ISupportInitialize)(this.colorShower)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorShowerStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorShowerRes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -275,7 +305,9 @@
         private Label labelHR;
         private Button bnSolve;
         private Button button1;
-        private PictureBox colorShower;
+        private PictureBox colorShowerStart;
         private Label label4;
+        private PictureBox colorShowerRes;
+        private Label label5;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualBasic.Devices;
+using System.Windows.Forms;
+
 namespace Lab_03_BV
 {
     public partial class Form1 : Form
@@ -5,11 +8,34 @@ namespace Lab_03_BV
         public Form1()
         {
             InitializeComponent();
+            txtH.Text = Properties.Settings.Default.H.ToString();
+            txtS.Text = Properties.Settings.Default.S.ToString();
+            txtV.Text = Properties.Settings.Default.V.ToString();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+        }
+        private void AutoColor()
+        {
+            try
+            {
+                var h = int.Parse(txtH.Text);
+                var s = int.Parse(txtS.Text);
+                var v = int.Parse(txtV.Text);
+                HSV hsv = new HSV(h, s, v);
+                RGB rgb1 = hsv.HSVToRGB();
+                hsv.CheckHSV();
+                colorShowerStart.BackColor = System.Drawing.Color.FromArgb(rgb1.R, rgb1.G, rgb1.B);
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+            catch (Exception)
+            {
+                return;
+            }
         }
         private void Calculate(int i)
         {
@@ -18,7 +44,9 @@ namespace Lab_03_BV
             var s = int.Parse(txtS.Text);
             var v = int.Parse(txtV.Text);
                 HSV hsv = new HSV(h, s, v);
+                RGB rgb1 = hsv.HSVToRGB();
                 hsv.CheckHSV();
+                colorShowerStart.BackColor = System.Drawing.Color.FromArgb(rgb1.R, rgb1.G, rgb1.B);
                 if (i == 1)
                 {
                     switch (cmbOperation.Text)
@@ -74,26 +102,27 @@ namespace Lab_03_BV
                     hsv.CheckHSV();
                     RGB rgb = hsv.HSVToRGB();
                     rgb.CheckRGB();
-                    labelHR.Text = "H";
-                    labelSG.Text = "S";
-                    labelVB.Text = "V";
+                    labelHR.Text = "H [0..360]";
+                    labelSG.Text = "S [0..100]";
+                    labelVB.Text = "V [0..100]";
                     txtResH.Text = hsv.H.ToString();
                     txtResS.Text = hsv.S.ToString();
                     txtResV.Text = hsv.V.ToString();
-                    colorShower.BackColor = System.Drawing.Color.FromArgb(rgb.R, rgb.G, rgb.B);
+                    colorShowerRes.BackColor = System.Drawing.Color.FromArgb(rgb.R, rgb.G, rgb.B);
                 }
                 else if (i == 2)
                 {
+                    colorShowerStart.BackColor = System.Drawing.Color.FromArgb(rgb1.R, rgb1.G, rgb1.B);
                     hsv.CheckHSV();
                     RGB rgb = hsv.HSVToRGB();
                     rgb.CheckRGB();
-                    labelHR.Text = "R";
-                    labelSG.Text = "G";
-                    labelVB.Text = "B";
+                    labelHR.Text = "R [0..255]";
+                    labelSG.Text = "G [0..255]";
+                    labelVB.Text = "B [0..255]";
                     txtResH.Text = rgb.R.ToString();
                     txtResS.Text = rgb.G.ToString();
                     txtResV.Text = rgb.B.ToString();
-                    colorShower.BackColor = System.Drawing.Color.FromArgb(rgb.R,rgb.G,rgb.B);
+                    colorShowerRes.BackColor = System.Drawing.Color.FromArgb(rgb.R,rgb.G,rgb.B);
                 }
             }
             catch(FormatException)
@@ -111,17 +140,17 @@ namespace Lab_03_BV
 
         private void txtH_TextChanged(object sender, EventArgs e)
         {
-            
+            AutoColor();
         }
 
         private void txtS_TextChanged(object sender, EventArgs e)
         {
-            
+            AutoColor();
         }
 
         private void txtV_TextChanged(object sender, EventArgs e)
         {
-            
+            AutoColor();
         }
 
         private void txtTerm_TextChanged(object sender, EventArgs e)
@@ -137,6 +166,11 @@ namespace Lab_03_BV
         private void button1_Click(object sender, EventArgs e)
         {
             Calculate(2);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
